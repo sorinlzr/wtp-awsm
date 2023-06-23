1d8c3dc3506397976532dcb55662114cf17eac7c
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-23 22:32:13 +0200

    Changed the logout router from post to get.

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -7,4 +7,4 @@ import { login, logout } from '../controllers/authController.js';
 export const authRouter = express.Router();
 
 authRouter.post("/login", login);
-authRouter.post("/logout", logout);
+authRouter.get("/logout", logout);
