bc83c201db386cae68ca54f9f8e10fcbef8fc94b
sorin.lzr <soso.laz@gmail.com>
2023-06-24 16:52:08 +0200

    fix issue with jwt token expiresIn

--- backend/src/util/secretToken.js
+++ backend/src/util/secretToken.js
@@ -9,7 +9,8 @@ export const createSecretToken = (payload) => {
         payload,
         process.env.JWT_SECRET,
         {
-            expiresIn: process.env.JWT_MAX_AGE
+            // is the value is a string, it will be interpreted in milliseconds. If the value is a number, it will be interpreted in seconds
+            expiresIn: process.env.JWT_MAX_AGE * 1
         }
     );
 }
