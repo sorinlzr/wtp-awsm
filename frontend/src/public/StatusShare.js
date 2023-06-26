9b1b8e3d9ecc1dba442b8a544f4386e547e438fe
JKardouh <judy.kardouh@stud.fh-campuswien.ac.at>
2023-06-27 00:16:53 +0200

    implement delete functionality

--- frontend/src/public/StatusShare.js
+++ frontend/src/public/StatusShare.js
@@ -96,9 +96,9 @@ function renderPosts(posts, users) {
           translationText = `Translate to ${translatedLanguage}`;
         }
 
-        postElement.innerHTML = `
+      postElement.innerHTML = `
       <div class="status-main">
-        <img src="${postingUser.avatar}" class="status-img alt=userAvatar" />
+        <img src="${postingUser.avatar}" class="status-img" alt="user avatar"/>
         <div class="album-detail">
           <div class="album-title"><strong>${postingUser.username}</strong> create new <span>post</span></div>
           <div class="album-date">${post.date}</div>
@@ -111,6 +111,8 @@ function renderPosts(posts, users) {
         ${post.labels.map(label => `<button class="hashtag-label">${label.label}</button>`).join('')}
       </div>
       <div class="album-actions">
+        <div class="button-container">
+        <div class="left-buttons">
         <a href="#" class="album-action">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
@@ -135,7 +137,29 @@ function renderPosts(posts, users) {
           ${translationText}    
         </button>
       </div>
-    `;
+      <div class="right-buttons">
+         ${
+            // Show the edit and delete buttons only if the current user is the owner of the post
+            post.user === currentUser._id
+              ? `
+              <a href="#" class="album-action">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brush" viewBox="0 0 16 16">
+                <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z"/>
+              </svg>Edit
+            </a>
+            <a href="#" onclick="deletePost('${post._id}')" class="album-action delete-button" data-post-id="${post._id}">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bucket" viewBox="0 0 16 16">
+                <path d="M2.522 5H2a.5.5 0 0 0-.494.574l1.372 9.149A1.5 1.5 0 0 0 4.36 16h7.278a1.5 1.5 0 0 0 1.483-1.277l1.373-9.149A.5.5 0 0 0 14 5h-.522A5.5 5.5 0 0 0 2.522 5zm1.005 0a4.5 4.5 0 0 1 8.945 0H3.527zm9.892 1-1.286 8.574a.5.5 0 0 1-.494.426H4.36a.5.5 0 0 1-.494-.426L2.58 6h10.838z"/>
+              </svg>Delete
+            </a>
+              `
+              : ''
+          }
+      </div>
+    </div>
+  </div>
+</div>
+`;
 
         container.appendChild(postElement);
 
@@ -188,3 +212,21 @@ function translatePost(event) {
 
 // Call fetchUsers() when the page loads
 window.addEventListener('load', fetchUsers);
+
+function deletePost(postId) {
+  console.log(`delete post called with post id: ${postId}`)
+  fetch(`/api/posts/${postId}`, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Post deleted successfully');
+        location.reload();
+      } else {
+        console.error('Error deleting post:', response.statusText);
+      }
+    })
+    .catch(error => {
+      console.error('Error deleting post:', error);
+    });
+}
